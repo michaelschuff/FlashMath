6b0d9b43d6c0213d648da252875cc3beea2acfd6
michaelschuff <mschuff0881@gmail.com>
2023-07-30 14:44:13 -0400

    comments

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import logo from './logo.svg';
-
-// import CardPage from './Card.js'
 
 // Add Routes Below:
-// import Login from './routes/LoginPage/LoginPage.js'
-// import MainCardDisplay from './routes/MainCardDisplay/Card.js'
 import EditorPage from './routes/EditorPage/EditorPage.js'
 import LoginPage from './routes/LoginPage/LoginPage.js'
 
