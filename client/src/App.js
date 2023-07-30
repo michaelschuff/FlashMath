aa4e80fcb09170424b1aae6863cbb3813e04797f
michaelschuff <mschuff0881@gmail.com>
2023-07-30 14:48:09 -0400

    more conflicts

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,21 @@
 import React from 'react'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 // import logo from './logo.svg';
-import EditorPage from './EditorPage/EditorPage.js'
-// import LoginPage from './LoginPage/LoginPage.js'
+// import CardPage from './Card.js'
+
+// Add Routes Below:
+import Login from './routes/LoginPage/LoginPage.js'
+import MainCardDisplay from './routes/MainCardDisplay/Card.js'
 
 function App () {
   return (
   // comment or uncomment the lines below
-
-    <EditorPage> </EditorPage>
-    // <LoginPage> </LoginPage>
+    <Router>
+      <Routes>
+        <Route path='/' id='Login' element={<Login/>} />
+        <Route path='/cards' id='MainCardDisplay' element={<MainCardDisplay/>} />
+      </Routes>
+    </Router>
   )
 }
 
