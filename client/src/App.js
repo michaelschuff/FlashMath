277903d07ede3f5138641c2d5a35168e6f8ff923
michaelschuff <mschuff0881@gmail.com>
2023-07-30 15:17:49 -0400

    conflicts

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 // import logo from './logo.svg';
-// import CardPage from './Card.js'
 
 // Add Routes Below:
 import Login from './routes/LoginPage/LoginPage.js'
