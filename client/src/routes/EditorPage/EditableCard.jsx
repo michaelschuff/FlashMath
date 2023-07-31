986de77eeecdebead60dabb9f8b141811089efa3
Yuyang Zheng <yuzheng5555@gmail.com>
2023-07-31 19:16:41 -0400

    stuff

--- client/src/routes/EditorPage/EditableCard.jsx
+++ client/src/routes/EditorPage/EditableCard.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 
 const EditableCard = ({ initialText, title, type }) => {
   const editableRef = useRef(null)
