36955268dcc3accbaa4b968a8564a28b1e34fbf9
Billy Merchan <billymerchan@gmail.com>
2023-07-31 20:13:27 -0400

    Fixing logo add

--- client/src/routes/SiteItems/Logo.jsx
+++ client/src/routes/SiteItems/Logo.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const Logo = () => {
   return (
     <div class='site-logo'>
-      <img src='favicon.ico' class='logo-img' />
+      <img src='./logo512.png' class='logo-img' />
       <text>FlashMath</text>
     </div>
   )
