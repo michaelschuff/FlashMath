3a8cb98a81933c375fa1f0603643bce7fd3bd8dc
Billy Merchan <billymerchan@gmail.com>
2023-08-01 22:16:36 -0400

    Fixing rest of jsx practice

--- client/src/routes/SiteItems/Menu.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const Menu = () => {
-  return (
-    <img src='https://icons.veryicon.com/png/o/miscellaneous/linear-icon-45/hamburger-menu-5.png' class='menu-icon' />
-  )
-}
