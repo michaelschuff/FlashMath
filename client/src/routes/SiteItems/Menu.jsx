2c02e2a5583ca557a56290e2ffe9b68068203062
Jun Song <40209659+Acewvrs@users.noreply.github.com>
2023-08-06 18:04:28 -0400

    replace menu bar with logout button

--- client/src/routes/SiteItems/Menu.jsx
+++ client/src/routes/SiteItems/Menu.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const Menu = () => {
   return (
-    <img src='https://icons.veryicon.com/png/o/miscellaneous/linear-icon-45/hamburger-menu-5.png' class='menu-icon' />
+    // <img src='https://icons.veryicon.com/png/o/miscellaneous/linear-icon-45/hamburger-menu-5.png' class='menu-icon' />
+    <Link to='/.' className='menu-icon'>
+      Logout
+    </Link>
   )
 }
